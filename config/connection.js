f58b6e1eea2357b4efdd329661f49dc702a120a2
sicknick2030 <nicholasjvolpe@gmail.com>
2018-03-06 21:32:47 -0800

    fixing heroku

--- config/connection.js
+++ config/connection.js
@@ -5,12 +5,13 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
  connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+	  host: "localhost",
+	  port: 3306,
+	  user: "root",
+	  password: "",
+	  database: "burgers_db"
+	});
+};
 
 connection.connect();
 // function(err) {
