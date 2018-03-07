a85588ab537b17ac9c3c64a4f9b471202fdbb2e3
sicknick2030 <nicholasjvolpe@gmail.com>
2018-03-06 21:29:59 -0800

    trying to fix heroku

--- config/connection.js
+++ config/connection.js
@@ -12,13 +12,14 @@ if (process.env.JAWSDB_URL) {
   database: "burgers_db"
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
+// function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
